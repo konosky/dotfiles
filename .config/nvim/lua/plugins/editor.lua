d4cea6607a6c14a1782a3911296dcf80f4b5582d
Konosuke Sakai <konosuke@konosuke.work>
2024-12-24 15:39:49 +0900

    feat(editor.lua): remove previous and next buffer keybindings and add neominimap plugin with comprehensive controls for better navigation and visualization in Neovim

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -183,9 +183,6 @@ return {
 
 			{ "<Leader>w", "<Cmd>write<CR>", desc = "Write" },
 
-			{ "<Leader>p", "<Cmd>bprevious<CR>", desc = "Previous" },
-			{ "<Leader>n", "<Cmd>bnext<CR>", desc = "Next" },
-
 			{ "<Leader>s", "<Cmd>split<CR>", desc = "Split horizontal" },
 			{ "<Leader>S", "<Cmd>vsplit<CR>", desc = "Split vertical" },
 
@@ -269,5 +266,48 @@ return {
 		keys = {
 			{ "<Leader>z", "<Cmd>ZenMode<CR>", desc = "Zen Mode" },
 		},
+	},
+	{
+		"Isrothy/neominimap.nvim",
+		enabled = true,
+		lazy = false,
+		keys = {
+			-- Global Minimap Controls
+			{ "<leader>nm", "<cmd>Neominimap toggle<cr>", desc = "Toggle global minimap" },
+			{ "<leader>no", "<cmd>Neominimap on<cr>", desc = "Enable global minimap" },
+			{ "<leader>nc", "<cmd>Neominimap off<cr>", desc = "Disable global minimap" },
+			{ "<leader>nr", "<cmd>Neominimap refresh<cr>", desc = "Refresh global minimap" },
+
+			-- Window-Specific Minimap Controls
+			{ "<leader>nwt", "<cmd>Neominimap winToggle<cr>", desc = "Toggle minimap for current window" },
+			{ "<leader>nwr", "<cmd>Neominimap winRefresh<cr>", desc = "Refresh minimap for current window" },
+			{ "<leader>nwo", "<cmd>Neominimap winOn<cr>", desc = "Enable minimap for current window" },
+			{ "<leader>nwc", "<cmd>Neominimap winOff<cr>", desc = "Disable minimap for current window" },
+
+			-- Tab-Specific Minimap Controls
+			{ "<leader>ntt", "<cmd>Neominimap tabToggle<cr>", desc = "Toggle minimap for current tab" },
+			{ "<leader>ntr", "<cmd>Neominimap tabRefresh<cr>", desc = "Refresh minimap for current tab" },
+			{ "<leader>nto", "<cmd>Neominimap tabOn<cr>", desc = "Enable minimap for current tab" },
+			{ "<leader>ntc", "<cmd>Neominimap tabOff<cr>", desc = "Disable minimap for current tab" },
+
+			-- Buffer-Specific Minimap Controls
+			{ "<leader>nbt", "<cmd>Neominimap bufToggle<cr>", desc = "Toggle minimap for current buffer" },
+			{ "<leader>nbr", "<cmd>Neominimap bufRefresh<cr>", desc = "Refresh minimap for current buffer" },
+			{ "<leader>nbo", "<cmd>Neominimap bufOn<cr>", desc = "Enable minimap for current buffer" },
+			{ "<leader>nbc", "<cmd>Neominimap bufOff<cr>", desc = "Disable minimap for current buffer" },
+
+			---Focus Controls
+			{ "<leader>nf", "<cmd>Neominimap focus<cr>", desc = "Focus on minimap" },
+			{ "<leader>nu", "<cmd>Neominimap unfocus<cr>", desc = "Unfocus minimap" },
+			{ "<leader>ns", "<cmd>Neominimap toggleFocus<cr>", desc = "Switch focus on minimap" },
+		},
+		init = function()
+			vim.opt.wrap = false
+			vim.opt.sidescrolloff = 36
+
+			vim.g.neominimap = {
+				auto_enable = true,
+			}
+		end,
 	},
 }
