02ac99e6681f96390db0ee8df090b57962d22253
Konosuke Sakai <konosuke@konosuke.work>
2024-12-30 07:11:14 +0900

    feat(jupyter.lua): add NotebookNavigator.nvim plugin for enhanced notebook navigation and cell management
    This change introduces the NotebookNavigator.nvim plugin, which provides
    key mappings for moving between cells and executing them. It also sets up
    dependencies and configuration options to improve the user experience
    when working with Jupyter notebooks in Neovim.

--- .config/nvim/lua/plugins/jupyter.lua
+++ .config/nvim/lua/plugins/jupyter.lua
@@ -9,4 +9,41 @@ return {
 		version = "0.2.0",
 		opts = {},
 	},
+	{
+		-- "GCBallesteros/NotebookNavigator.nvim",
+		"konosuke-sakai/NotebookNavigator.nvim",
+		branch = "bugfix/remove-trailing-marker",
+		keys = {
+			{
+				"]h",
+				function()
+					require("notebook-navigator").move_cell("d")
+				end,
+			},
+			{
+				"[h",
+				function()
+					require("notebook-navigator").move_cell("u")
+				end,
+			},
+			{ "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
+			{ "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
+		},
+		dependencies = {
+			"echasnovski/mini.comment",
+			"hkupty/iron.nvim",
+			"anuvyklack/hydra.nvim",
+		},
+		event = "VeryLazy",
+		config = function()
+			local nn = require("notebook-navigator")
+			nn.setup({
+				cell_markers = {
+					markdown = "```",
+				},
+				activate_hydra_keys = "<leader>h",
+				repl_provider = "molten",
+			})
+		end,
+	},
 }
