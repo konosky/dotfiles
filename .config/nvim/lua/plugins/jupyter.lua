2330419628d7e1cd38c874e816fc94544f4c53ac
Konosuke Sakai <konosuke@konosuke.work>
2024-12-30 07:11:24 +0900

    feat(jupyter.lua): add initialization function to set molten_image_provider to image.nvim for better image handling in Jupyter notebooks

--- .config/nvim/lua/plugins/jupyter.lua
+++ .config/nvim/lua/plugins/jupyter.lua
@@ -3,6 +3,9 @@ return {
 		"benlubas/molten-nvim",
 		version = "^1.0.0",
 		build = ":UpdateRemotePlugins",
+		init = function()
+			vim.g.molten_image_provider = "image.nvim"
+		end,
 	},
 	{
 		"goerz/jupytext.nvim",
