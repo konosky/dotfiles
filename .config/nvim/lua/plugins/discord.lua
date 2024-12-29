40a1d26fb73091e3220e952a9cf1fcdba0460f6d
Konosuke Sakai <konosuke@konosuke.work>
2024-12-30 02:43:46 +0900

    feat(discord.lua): add functions to retrieve playerctl status and metadata for better integration with media players refactor(discord.lua): update workspace_text option to include playerctl information for enhanced user experience

--- .config/nvim/lua/plugins/discord.lua
+++ .config/nvim/lua/plugins/discord.lua
@@ -1,7 +1,45 @@
+local function get_playerctl_status()
+	local handle = io.popen("playerctl status")
+	if not handle then
+		return ""
+	end
+	return handle:read("*l")
+end
+
+local function get_playerctl_metadata_title()
+	local handle = io.popen("playerctl metadata title")
+	if not handle then
+		return ""
+	end
+	return handle:read("*l")
+end
+
+local function get_playerctl_metadata_artist()
+	local handle = io.popen("playerctl metadata artist")
+	if not handle then
+		return ""
+	end
+	return handle:read("*l")
+end
+
+local function get_player_text()
+	return get_playerctl_metadata_title() .. " - " .. get_playerctl_metadata_artist()
+end
+
+local function workspace_text_option(...)
+	if get_playerctl_status() == "" then
+		return ...
+	end
+
+	return get_player_text() .. " | " .. ...
+end
+
 return {
 	{
 		"IogaMaster/neocord",
 		event = "VeryLazy",
-		config = true,
+		opts = {
+			workspace_text = workspace_text_option,
+		},
 	},
 }
