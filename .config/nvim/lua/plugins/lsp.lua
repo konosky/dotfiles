05e254f0040df7e53bf9fdee2f7feb4f5f9f25b0
Konosuke Sakai <konosuke@konosuke.work>
2024-12-30 23:42:27 +0900

    feat(lsp.lua): add friendly-snippets plugin to enhance snippet support in Neovim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -59,6 +59,7 @@ return {
 			{ "hrsh7th/cmp-cmdline" },
 			{ "hrsh7th/cmp-vsnip" },
 			{ "hrsh7th/vim-vsnip" },
+			{ "rafamadriz/friendly-snippets" },
 		},
 		config = function()
 			local cmp = require("cmp")
