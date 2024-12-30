68411f32f7fcbb2f44daf6e4ae5efc23cdb1377c
Konosuke Sakai <konosuke@konosuke.work>
2024-12-30 23:49:59 +0900

    feat(lsp.lua): add has_words_before function to enhance completion behavior fix(lsp.lua): modify Tab key mapping to improve completion experience when words are present

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -64,6 +64,14 @@ return {
 		config = function()
 			local cmp = require("cmp")
 
+			local has_words_before = function()
+				if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+					return false
+				end
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+			end
 			cmp.setup({
 				expand = function(args)
 					vim.fn["vsnip#anonymous"](args.body)
@@ -74,7 +82,13 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = vim.schedule_wrap(function(fallback)
+						if cmp.visible() and has_words_before() then
+							cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+						else
+							fallback()
+						end
+					end),
 				}),
 				sources = cmp.config.sources({
 					{ name = "copilot" },
