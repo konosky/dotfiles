e0607ccefdf0317de2b8ae034b27a738767575ab
Konosuke Sakai <konosuke@konosuke.work>
2025-01-04 18:50:58 +0900

    feat(docs): add README.md to provide project overview and setup instructions fix(symlink.sh): exclude README.md from symlink creation to prevent conflicts

--- symlink.sh
+++ symlink.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fd -H -E "$(basename "$0")" -E .git -E LICENSE -t f -x mkdir -p -v "${HOME}/{//}" \; -x ln -f -s -v "${PWD}/{}" "${HOME}/{}"
+fd -H -E "$(basename "$0")" -E .git -E LICENSE -E README.md -t f -x mkdir -p -v "${HOME}/{//}" \; -x ln -f -s -v "${PWD}/{}" "${HOME}/{}"
